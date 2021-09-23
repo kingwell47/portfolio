c065cfdbb59e157bba8490c84df9b3374cc6a170
kingwell47 <joeldoctor47@gmail.com>
2021-09-23 18:07:17 +0800

    done some works mapping

---
+++ components/Works/images/index.js
@@ -0,0 +1,17 @@
+const IMAGES = {
+  calculator: require("./calculator.png").default,
+  pomodoro: require("./pomodoro.png").default,
+  room: require("./room.png").default,
+  url_shortening: require("./url_shortening.png").default,
+  html: require("../../Hero/icons/html.png").default,
+  css: require("../../Hero/icons/css.png").default,
+  javascript: require("../../Hero/icons/javascript.png").default,
+  zendesk: require("../../Hero/icons/zendesk.png").default,
+  react: require("../../Hero/icons/react.png").default,
+  excel: require("../../Hero/icons/excel.png").default,
+  inkscape: require("../../Hero/icons/inkscape.png").default,
+  photoshop: require("../../Hero/icons/photoshop.png").default,
+  resolve: require("../../Hero/icons/resolve.png").default,
+};
+
+export default IMAGES;
--- components/Works/index.jsx
+++ components/Works/index.jsx
@@ -1,3 +1,8 @@
+import image from "next/image";
+import Image from "next/image";
+import images from "./images";
+import styles from "./Works.module.scss";
+
 //Works will have tabs that cycle between different displays
 
 //Tab 1 - WebDev
@@ -8,49 +13,65 @@
 
 //
 
+const CONTENT = [
+  {
+    image: images.url_shortening,
+    title: "URL Shortening",
+    text: "Responsive landing page with working URL Shortening link using API",
+    tech: [
+      { logo: images.html, name: "html" },
+      { logo: images.css, name: "css" },
+      { logo: images.javascript, name: "javascript" },
+      { logo: images.react, name: "react" },
+    ],
+    live: "http://kingwell47.github.io/url-shortnening",
+    git: "https://github.com/kingwell47/url-shortnening",
+  },
+  {
+    image: images.calculator,
+    title: "JavaScript Calculator",
+    text: "JavaScript Calculator built with React",
+    tech: [
+      { logo: images.html, name: "html" },
+      { logo: images.css, name: "css" },
+      { logo: images.javascript, name: "javascript" },
+      { logo: images.react, name: "react" },
+    ],
+    live: "https://kingwell47.github.io/javascript-calculator/",
+    git: "https://github.com/kingwell47/javascript-calculator",
+  },
+];
+
 function Works() {
+  function TechList(props) {
+    return (
+      <>
+        {props.items.map((item, index) => {
+          return (
+            <div key={index} className={styles.logo}>
+              <Image src={item.logo} alt={item.name} />
+            </div>
+          );
+        })}
+      </>
+    );
+  }
+  function Card(props) {
+    return (
+      <div>
+        <Image src={props.content.image} alt={props.content.title} />
+        <p>{props.content.text}</p>
+        <TechList items={props.content.tech} />
+      </div>
+    );
+  }
+
   return (
     <section>
-      <div>
-        <h2>Project Title</h2>
-        <p>Project Description</p>
-        <div>Technologies Used</div>
-      </div>
-      <div>
-        <h2>Project Title</h2>
-        <p>Project Description</p>
-        <div>Technologies Used</div>
-      </div>
-      <div>
-        <h2>Project Title</h2>
-        <p>Project Description</p>
-        <div>Technologies Used</div>
-      </div>
-      <div>
-        <h2>Project Title</h2>
-        <p>Project Description</p>
-        <div>Technologies Used</div>
-      </div>
-      <div>
-        <h2>Project Title</h2>
-        <p>Project Description</p>
-        <div>Technologies Used</div>
-      </div>
-      <div>
-        <h2>Project Title</h2>
-        <p>Project Description</p>
-        <div>Technologies Used</div>
-      </div>
-      <div>
-        <h2>Project Title</h2>
-        <p>Project Description</p>
-        <div>Technologies Used</div>
-      </div>
-      <div>
-        <h2>Project Title</h2>
-        <p>Project Description</p>
-        <div>Technologies Used</div>
-      </div>
+      <h2>Works</h2>
+      {CONTENT.map((item, index) => {
+        return <Card content={item} key={index} />;
+      })}
     </section>
   );
 }
