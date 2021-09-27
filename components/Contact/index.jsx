f8a456fd674725627762de8c5ff034691c83f4ea
kingwell47 <joeldoctor47@gmail.com>
2021-09-27 18:07:01 +0800

    modified contact page further

--- components/Contact/index.jsx
+++ components/Contact/index.jsx
@@ -1,13 +1,24 @@
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
+import Image from "next/image";
 import emailjs from "emailjs-com";
 import styles from "./Contact.module.scss";
+import image from "./image.jpg";
 
 //Contact should be a popup page whenever the button is clicked
 //Implement Capcha and make leaving their email address optional
 //Let the visitor know what form solution I am using so they are informed
 
 function Contact() {
+  const [sent, setSent] = useState(false);
   const form = useRef();
+
+  const handleSent = () => {
+    setSent(true);
+  };
+
+  const handleChange = () => {
+    setSent(false);
+  };
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -28,55 +39,72 @@ function Contact() {
         }
       );
     e.target.reset();
+    handleSent();
   };
   return (
     <section className={styles.contact}>
       <h1 className={styles.title}>Contact Me</h1>
-      <form
-        ref={form}
-        onSubmit={sendEmail}
-        className={`${styles.form} container`}>
-        <label className={styles.label} htmlFor='user_name'>
-          Name
-        </label>
-        <input
-          className={styles.input}
-          type='text'
-          name='user_name'
-          title='Name'
-          required
-        />
-        <label className={styles.label} htmlFor='user_email'>
-          Email
-        </label>
-        <input
-          className={styles.input}
-          type='email'
-          name='user_email'
-          title='Email'
-          required
-        />
-        <label className={styles.label} htmlFor='subject'>
-          Subject
-        </label>
-        <input
-          className={styles.input}
-          type='text'
-          name='subject'
-          title='Subject'
-          required
-        />
-        <label className={styles.label} htmlFor='message'>
-          Message
-        </label>
-        <textarea
-          className={styles.text_area}
-          name='message'
-          title='Message'
-          required
-        />
-        <input type='submit' value='Send' className={`${styles.button} btn`} />
-      </form>
+      <div className={styles.wrapper}>
+        <div className='hide-for-mobile'>
+          <Image src={image} alt='' />
+        </div>
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className={`${styles.form} container`}>
+          <label className={styles.label} htmlFor='user_name'>
+            Name
+          </label>
+          <input
+            className={styles.input}
+            type='text'
+            name='user_name'
+            title='Name'
+            onChange={handleChange}
+            required
+          />
+          <label className={styles.label} htmlFor='user_email'>
+            Email
+          </label>
+          <input
+            className={styles.input}
+            type='email'
+            name='user_email'
+            title='Email'
+            onChange={handleChange}
+            required
+          />
+          <label className={styles.label} htmlFor='subject'>
+            Subject
+          </label>
+          <input
+            className={styles.input}
+            type='text'
+            name='subject'
+            title='Subject'
+            onChange={handleChange}
+            required
+          />
+          <label className={styles.label} htmlFor='message'>
+            Message
+          </label>
+          <textarea
+            className={styles.text_area}
+            name='message'
+            title='Message'
+            onChange={handleChange}
+            required
+          />
+          <span className={styles.confirmation}>
+            {sent ? "Thanks for the message!" : ""}
+          </span>
+          <input
+            type='submit'
+            value='Send'
+            className={`${styles.button} btn`}
+          />
+        </form>
+      </div>
     </section>
   );
 }
