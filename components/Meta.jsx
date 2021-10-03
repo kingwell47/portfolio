e181a7047dc34d173a5b38ad46ac9065361d3f9b
kingwell47 <joeldoctor47@gmail.com>
2021-10-03 18:19:01 +0800

    added og image

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -7,6 +7,7 @@ const Meta = ({ title, keywords, description }) => {
       <meta name='keywords' content={keywords} />
       <meta name='description' content={description} />
       <meta charSet='utf-8' />
+      <meta property='og:image' content='https://joeldoctor.com/ogimage.jpeg' />
       <link rel='icon' href='/logo.png' />
       <title>{title}</title>
     </Head>
